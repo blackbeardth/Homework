34a0833a00dee85b6a152d8f978869c9efc6f114
Chirag Wadhwa <70256225+chiragwad@users.noreply.github.com>
2024-05-07 16:37:20 +0530

    Update s2q4.R

--- s2q4.R
+++ s2q4.R
@@ -5,8 +5,10 @@ data_trans <- read.transactions("./data/bakery/1000-out1.csv",sep=",", cols = 1,
 
 id <- c(0:49)
 name <- factor(c("Chocolate Cake","Lemon Cake","Casino Cake","Opera Cake","Strawberry Cake","Truffle Cake","Chocolate Eclair","Coffee Eclair","Vanilla Eclair","Napoleon Cake","Almond Tart","Apple Pie","Apple Tart","Apricot Tart","Berry Tart","Blackberry Tart","Blueberry Tart","Chocolate Tart","Cherry Tart","Lemon Tart","Pecan Tart","Ganache Cookie","Gongolals Cookie","Raspberry Cookie","Lemon Cookie","Chocolate Meringue","Vanilla Meringue","Marzipan Cookie","Tuile Cookie","Walnut Cookie","Almond Croissant","Apple Croissant","Apricot Croissant","Cheese Croissant","Chocolate Croissant","Apricot Danish","Apple Dansh","Almond Twist","Almond Bear Claw","Blueberry Danish","Lemonade","Raspberry Lemonade","Orange Juice","Green Tea","Bottled Water","Hot Coffee","Chocolate Coffee","Vanilla Frappucino","Cherry Soda","Single Espresso"))
+# creates a new table with columns id and name
 goods_name <- data.frame(id, name)
 
+# This line updates the labels of items in the transaction data with their corresponding names from the goods_name data frame.
 data_trans@itemInfo$labels <- goods_name$name[match(data_trans@itemInfo$labels,goods_name$id)] 
 
 itemFrequencyPlot(data_trans,topN=10,type="absolute")
