3f51d81d8dac00c167fca6fce71e895c272b05ab
Chirag Wadhwa <70256225+chiragwad@users.noreply.github.com>
2024-05-14 18:39:33 +0530

    Update and rename p9.pl to conc.pl

--- conc.pl
+++ conc.pl
@@ -1,4 +1,4 @@
-% program to implement conc(L1,L2,L3) where L2 is the kust to be
+% program to implement conc(L1,L2,L3) where L2 is the list to be
 % appended with L1 to get the resulted list L3.
 
 conc([],L,L).
