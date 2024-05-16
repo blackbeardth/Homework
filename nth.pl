ae28b56a3a0e83ab0e78976d3d0ed464b73cbf7f
Chirag Wadhwa <70256225+chiragwad@users.noreply.github.com>
2024-05-16 22:02:22 +0530

    Delete nth.pl

--- nth.pl
+++
@@ -1,5 +0,0 @@
-% program to implement nth_element(N,L,X) where N is the desired
-% position, L is a list and X represents the Nth element of L.
-
-nElement(1,[H|_],H).
-nElement(N,[_|T],X):- N1 is N-1, nElement(N1,T,X).
