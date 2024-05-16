9ec35374923595433afcbe4191444113524558c4
Chirag Wadhwa <70256225+chiragwad@users.noreply.github.com>
2024-05-16 22:01:54 +0530

    Delete sl.pl

--- sl.pl
+++
@@ -1,6 +0,0 @@
-% Write a Program in PROLOG to implement sublist(S, L) that checks whether the list S is the
-% sublist of list L or not. (Check for sequence or the part in the same order).
-
-sublist([],[]).
-sublist([First|Rest],[First|Sub]):- sublist(Rest,Sub).
-sublist([_|Rest],Sub):-sublist(Rest,Sub).
